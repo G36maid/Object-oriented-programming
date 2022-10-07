612905f952a37c998cd49a0036bfc10087a39ea3
G36maid <53391375+G36maid@users.noreply.github.com>
2022-10-07 15:09:20 +0800

    QUIZ 4

---
+++ 106_Quiz4/106_QUiz4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _106_Quiz4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle tri = new Triangle();
+            string[] tokens;
+            while (true)
+            {
+                Console.WriteLine("請依序輸入三角形的三個頂點");
+                for (int i = 0; i <= 2; i++)
+                {
+                    Console.Write("(x" + (i + 1).ToString() + ", y" + (i + 1).ToString()+ ")= ");
+                    tokens = Console.ReadLine().Split();
+                    tri.XCoord[i] = Double.Parse(tokens[0]);
+                    tri.YCoord[i] = Double.Parse(tokens[1]);
+                }
+                if (!tri.isValid())
+                    Console.WriteLine("Not a Triangle, Please Input the new coordinates!");
+                else
+                {
+                    Console.WriteLine("Perimeter=" + tri.Perimeter().ToString());
+                    Console.WriteLine("Area=" + tri.Area().ToString());
+                    Console.WriteLine("Radius of Circumcircle=" + tri.RadiusOfCircumcircle().ToString());
+                    if (tri.isRight())
+                        Console.WriteLine("This is a right triangle!");
+                }
+                Console.Write("Again?  ");
+                String check = Console.ReadLine();
+                if ((check != "y") && (check != "Y"))
+                    break;
+            }
+            Console.WriteLine("End of Program!");
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ 106_Quiz4/106_QUiz4/Triangle.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _106_Quiz4
+{
+    class Triangle
+    {
+        public double[] XCoord = new double[3];
+        public double[] YCoord = new double[3];
+        public const double Tol= 1e-10;
+        private void SideLengths(double[] side)
+        {
+            //side[0] = Math.Sqrt((XCoord[0] - XCoord[1]) * (XCoord[0] - XCoord[1]) + (YCoord[0] - YCoord[1]) * (YCoord[0] - YCoord[1]));
+            //side[1] = Math.Sqrt((XCoord[1] - XCoord[2]) * (XCoord[1] - XCoord[2]) + (YCoord[1] - YCoord[2]) * (YCoord[1] - YCoord[2]));
+            //side[2] = Math.Sqrt((XCoord[2] - XCoord[0]) * (XCoord[2] - XCoord[0]) + (YCoord[2] - YCoord[0]) * (YCoord[2] - YCoord[0]));
+            for (int i=0;i<3;i++)
+            {
+                side[i] = Math.Sqrt((XCoord[i] - XCoord[(i+1)%3]) * (XCoord[i] - XCoord[(i + 1) % 3]) + (YCoord[i] - YCoord[(i + 1) % 3]) * (YCoord[i] - YCoord[(i + 1) % 3]));
+            }
+        }
+        public double Perimeter()
+        {
+            double[] s = new double[3];
+            SideLengths(s);
+            double sum = 0;
+
+            foreach (var element in s)
+                sum += element;
+
+            return (sum);
+        }
+        public double Area()
+        {
+            double[] s = new double[3];
+            SideLengths(s);
+            double p = Perimeter() / 2;
+            return Math.Sqrt(p * (p - s[0]) * (p - s[1]) * (p - s[2]));
+        }
+        public bool isValid()
+        {
+            double[] s = new double[3];
+            SideLengths(s);
+            if ((Math.Abs(s[0] + s[1] - s[2]) < Tol) || (Math.Abs(s[1] + s[2] - s[0]) < Tol) || (Math.Abs(s[2] + s[0] - s[1]) < Tol))
+                return false;
+            return true;
+        }
+        public bool isRight()
+        {
+            double[] s = new double[3];
+            SideLengths(s);
+            if ((Math.Abs(s[0] * s[0] + s[1] * s[1] - s[2] * s[2]) < Tol) || (Math.Abs(s[1] * s[1] + s[2] * s[2] - s[0] * s[0]) < Tol) || (Math.Abs(s[0] * s[0] + s[2] * s[2] - s[1] * s[1]) < Tol))
+                return true;
+            return false;
+        }
+        public double RadiusOfCircumcircle()
+        {
+            double[] s = new double[3];
+            SideLengths(s);
+            double CosAlpha = (s[1] * s[1] + s[2] * s[2] - s[0] * s[0]) / (2 * s[1] * s[2]);
+            double SinAlpha = Math.Sqrt(1 - CosAlpha * CosAlpha);
+            return 0.5 * s[0] / SinAlpha;
+        }
+    }
+}
