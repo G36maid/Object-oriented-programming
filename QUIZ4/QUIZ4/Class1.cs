70fc82c1c9bea9dc9dba8eb8ee5f6da0ec200207
G36maid <53391375+G36maid@users.noreply.github.com>
2022-10-07 20:42:08 +0800

    Add files via upload

---
+++ QUIZ4/QUIZ4/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static QUIZ3.Triangle;
+
+namespace QUIZ3
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle tri = new Triangle();
+            while (true)
+            {
+                string str;
+                Console.WriteLine("請輸入三個點的平面座標，並計算其特性");
+                Console.WriteLine("(x1,y1)=");
+                str = Console.ReadLine();
+                tri.Px[1] = double.Parse(str);
+                str = Console.ReadLine();
+                tri.Py[1] = double.Parse(str);
+
+                Console.WriteLine("(x2,y2)=");
+                str = Console.ReadLine();
+                tri.Px[2] = double.Parse(str);
+                str = Console.ReadLine();
+                tri.Py[2] = double.Parse(str);
+
+                Console.WriteLine("(x3,y3)=");
+                str = Console.ReadLine();
+                tri.Px[3] = double.Parse(str);
+                str = Console.ReadLine();
+                tri.Py[3] = double.Parse(str);
+
+                if (!tri.IsValid())
+                {
+                    Console.WriteLine("這三點不能組成三角形！");
+
+                }
+                else
+                {
+                    Console.WriteLine("Perimeter=");
+                    Console.WriteLine(tri.Perimeter());
+                    Console.WriteLine("Area=");
+                    Console.WriteLine(tri.Area());
+                    Console.WriteLine("Radius Of Circumcircle=");
+                    Console.WriteLine(tri.RadiusOfCircumcircle());
+
+                    if (tri.IsRight())
+                    {
+                        Console.WriteLine("是直角三角形!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("非直角三角形!");
+                    }
+                }
+                Console.WriteLine("Again?");
+                string st = Console.ReadLine();
+                if (st == "n")
+                {
+                    break;
+                }
+                else if (st == "y")
+                {
+                    continue;
+                }
+
+
+
+            }
+            Console.ReadKey();
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ QUIZ4/QUIZ4/Triangle.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUIZ3
+{
+    class Triangle
+    {
+        public double[] Px = { 0, 0, 0,0 };
+        public double[] Py = { 0, 0, 0,0 };
+
+
+        private double Side(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+
+        }
+
+
+        public bool IsValid()// 檢查三角形是否合法
+        {
+            double[] s = { 0, 0, 0, 0,0};
+            s[1] = Side(Px[1], Py[1], Px[2], Py[2]);
+            s[2] = Side(Px[3], Py[3], Px[2], Py[2]);
+            s[3] = Side(Px[1], Py[1], Px[3], Py[3]);
+            s[4] = s[1] + s[2] + s[3];
+
+            if (s[1] + s[2] <= s[3] || s[2] + s[3] <= s[1] || s[1] + s[3] <= s[2])
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public double Perimeter()// 計算周長
+        {
+            double[] s = { 0, 0, 0, 0,0 };
+            s[1] = Side(Px[1], Py[1], Px[2], Py[2]);
+            s[2] = Side(Px[3], Py[3], Px[2], Py[2]);
+            s[3] = Side(Px[1], Py[1], Px[3], Py[3]);
+            s[4] = s[1] + s[2] + s[3];
+            return s[4];
+        }
+        public double Area()// 計算面積
+        {
+            double[] s = { 0, 0, 0, 0,0 };
+            s[1] = Side(Px[1], Py[1], Px[2], Py[2]);
+            s[2] = Side(Px[3], Py[3], Px[2], Py[2]);
+            s[3] = Side(Px[1], Py[1], Px[3], Py[3]);
+            s[4] = s[1] + s[2] + s[3];
+            s[4] = s[4] / 2;
+            return Math.Sqrt((s[4] - s[1]) * (s[4] - s[2]) * (s[4] - s[3]) * s[4]);
+        }
+
+        public double RadiusOfCircumcircle()// 計算外接圓半徑
+        {
+            double[] s = { 0, 0, 0, 0,0 };
+            s[1] = Side(Px[1], Py[1], Px[2], Py[2]);
+            s[2] = Side(Px[3], Py[3], Px[2], Py[2]);
+            s[3] = Side(Px[1], Py[1], Px[3], Py[3]);
+            s[4] = s[1] + s[2] + s[3];
+            double cosA = (s[1] * s[1] + s[2] * s[2] - s[3] * s[3]) / (2 * s[1] * s[2]);
+            double sinA = Math.Sqrt(1 - cosA * cosA);
+            return s[3] / (2 * sinA);
+
+        }
+        public bool IsRight()// 檢查三角形是否為直角
+        {
+            double[] s = { 0, 0, 0, 0,0 };
+            s[1] = Side(Px[1], Py[1], Px[2], Py[2]);
+            s[2] = Side(Px[3], Py[3], Px[2], Py[2]);
+            s[3] = Side(Px[1], Py[1], Px[3], Py[3]);
+            s[4] = s[1] + s[2] + s[3];
+            if (s[1] * s[1] + s[2] * s[2] - s[3] * s[3] < 1e-10 || s[1] * s[1] + s[3] * s[3] - s[2] * s[2] < 1e-10 || s[3] * s[3] + s[2] * s[2] - s[1] * s[1] < 1e-10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
