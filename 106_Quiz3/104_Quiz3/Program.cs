e2a2607a0a2c253ea6af5abeb5a958ac5a22614e
G36maid <53391375+G36maid@users.noreply.github.com>
2022-10-07 15:08:17 +0800

    QUIZˇ

---
+++ 106_Quiz3/104_Quiz3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _106_Quiz3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens;
+            Triangle tri=new Triangle();
+            while (true)
+            {
+                Console.WriteLine("請輸入三角形的三個頂點座標，並計算其特性");
+                Console.Write("(x1,y1)= ");
+                tokens = Console.ReadLine().Split();
+                tri.Point1X = Double.Parse(tokens[0]);
+                tri.Point1Y = Double.Parse(tokens[1]);
+                Console.Write("(x2,y2)= ");
+                tokens = Console.ReadLine().Split();
+                tri.Point2X = Double.Parse(tokens[0]);
+                tri.Point2Y = Double.Parse(tokens[1]);
+                Console.Write("(x3,y3)= ");
+                tokens = Console.ReadLine().Split();
+                tri.Point3X = Double.Parse(tokens[0]);
+                tri.Point3Y = Double.Parse(tokens[1]);
+
+                if (!tri.isValid())
+                {
+                    Console.WriteLine("這三點不能組成三角形!");
+                }
+                else
+                {
+                    Console.WriteLine("Perimeter=" + tri.Perimeter().ToString());
+                    Console.WriteLine("Area=" + tri.Area().ToString());
+                    Console.WriteLine("Radius of Circumcircle=" + tri.RadiusOfCircumcircle().ToString());
+                    if (tri.isRight())
+                        Console.WriteLine("直角三角形!");
+                    else
+                        Console.WriteLine("非直角三角形!");
+                }
+                Console.WriteLine("Again?  ");
+                String check = Console.ReadLine();
+                if ((check != "y")&&(check!="Y"))
+                    break;
+            }
+            Console.WriteLine("End of Program!");
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ 106_Quiz3/104_Quiz3/Triangle.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _106_Quiz3
+{
+    class Triangle
+    {
+        public double Point1X;
+        public double Point1Y;
+        public double Point2X;
+        public double Point2Y;
+        public double Point3X;
+        public double Point3Y;
+
+        private double SideLength(double X1, double Y1, double X2, double Y2)
+        {
+            return Math.Sqrt((X1 - X2) * (X1 - X2) + (Y1 - Y2) * (Y1 - Y2));
+        }
+        public double Perimeter()
+        {
+
+            double s1 = SideLength(Point1X, Point1Y, Point2X, Point2Y);
+            double s2 = SideLength(Point2X, Point2Y, Point3X, Point3Y);
+            double s3 = SideLength(Point3X, Point3Y, Point1X, Point1Y);
+
+            return (s1 + s2 + s3);
+        }
+        public double Area()
+        {
+            double s1 = SideLength(Point1X, Point1Y, Point2X, Point2Y);
+            double s2 = SideLength(Point2X, Point2Y, Point3X, Point3Y);
+            double s3 = SideLength(Point3X, Point3Y, Point1X, Point1Y);
+            double s = (s1 + s2 + s3) / 2;
+            return Math.Sqrt(s * (s - s1) * (s - s2) * (s - s3));
+        }
+        public bool isValid()
+        {
+            double s1 = SideLength(Point1X, Point1Y, Point2X, Point2Y);
+            double s2 = SideLength(Point2X, Point2Y, Point3X, Point3Y);
+            double s3 = SideLength(Point3X, Point3Y, Point1X, Point1Y);
+            if ((s1 + s2 <= s3) || (s2 + s3 <= s1) || (s1 + s3 <= s2))
+                return false;
+            return true;
+        }
+        public bool isRight()
+        {
+            double s1 = SideLength(Point1X, Point1Y, Point2X, Point2Y);
+            double s2 = SideLength(Point2X, Point2Y, Point3X, Point3Y);
+            double s3 = SideLength(Point3X, Point3Y, Point1X, Point1Y);
+            if ((s1 * s1 + s2 * s2 - s3 * s3 < 1e-10) || (s2 * s2 + s3 * s3 - s1 * s1 < 1e-10) || (s1 * s1 + s3 * s3 - s2 * s2 < 1e-10))
+                return true;
+            return false;
+        }
+        public double RadiusOfCircumcircle()
+        {
+            double s1 = SideLength(Point1X, Point1Y, Point2X, Point2Y);
+            double s2 = SideLength(Point2X, Point2Y, Point3X, Point3Y);
+            double s3 = SideLength(Point3X, Point3Y, Point1X, Point1Y);
+            double CosAlpha = (s2 * s2 + s3 * s3 - s1 * s1) / (2 * s2 * s3);
+            double SinAlpha = Math.Sqrt(1 - CosAlpha * CosAlpha);
+            return 0.5 * s1 / SinAlpha;
+        }
+
+    }
+}
