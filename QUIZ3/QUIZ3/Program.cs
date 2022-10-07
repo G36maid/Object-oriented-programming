855b180b2ea7c3bb00aa4d5652c4e3b8f71e2e06
G36maid <53391375+G36maid@users.noreply.github.com>
2022-10-07 20:41:41 +0800

    Add files via upload

---
+++ QUIZ3/QUIZ3/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static QUIZ3.Triangle;
+
+namespace QUIZ3
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle tri = new Triangle();
+            while (true)
+            {
+                string str;
+                Console.WriteLine("請輸入三個點的平面座標，並計算其特性");
+                Console.WriteLine("(x1,y1)=");
+                str = Console.ReadLine();
+                tri.Point1X = double.Parse(str);
+                str = Console.ReadLine();
+                tri.Point1Y = double.Parse(str);
+
+                Console.WriteLine("(x2,y2)=");
+                str = Console.ReadLine();
+                tri.Point2X = double.Parse(str);
+                str = Console.ReadLine();
+                tri.Point2Y = double.Parse(str);
+
+                Console.WriteLine("(x3,y3)=");
+                str = Console.ReadLine();
+                tri.Point3X = double.Parse(str);
+                str = Console.ReadLine();
+                tri.Point3Y = double.Parse(str);
+
+                if (!tri.IsValid())
+                {
+                    Console.WriteLine("這三點不能組成三角形！");
+                
+                }
+                else
+                {
+                    Console.WriteLine("Perimeter=");
+                    Console.WriteLine(tri.Perimeter());
+                    Console.WriteLine("Area=");
+                    Console.WriteLine(tri.Area());
+                    Console.WriteLine("Radius Of Circumcircle=");
+                    Console.WriteLine(tri.RadiusOfCircumcircle());
+
+                    if (tri.IsRight())
+                    {
+                        Console.WriteLine("是直角三角形!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("非直角三角形!");
+                    }
+                }
+                Console.WriteLine("Again?");
+                string st = Console.ReadLine();
+                if (st == "n")
+                {
+                    break;
+                }else if (st == "y")
+                {
+                    continue;
+                }
+               
+
+
+            }
+            Console.ReadKey();
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ QUIZ3/QUIZ3/Triangle.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUIZ3
+{
+    class Triangle
+    {
+        public double Point1X;
+        public double Point1Y;
+        public double Point2X;
+        public double Point2Y;
+        public double Point3X;
+        public double Point3Y;
+        
+        private double Side(double x1,double y1,double x2,double y2)
+        {
+            return Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+
+        }
+
+
+        public bool IsValid()// 檢查三角形是否合法
+        {
+            double s1 = Side(Point1X, Point1Y, Point2X, Point2Y);
+            double s2 = Side(Point3X, Point3Y, Point2X, Point2Y);
+            double s3 = Side(Point1X, Point1Y, Point3X, Point3Y);
+            if (s1+s2<=s3 || s2+s3<=s1 || s1+s3<=s2)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public double Perimeter()// 計算周長
+        {
+            double s1 = Side(Point1X, Point1Y, Point2X, Point2Y);
+            double s2 = Side(Point3X, Point3Y, Point2X, Point2Y);
+            double s3 = Side(Point1X, Point1Y, Point3X, Point3Y);
+            return (s1 + s2 + s3);
+        }
+        public double Area()// 計算面積
+        {
+            double s1 = Side(Point1X, Point1Y, Point2X, Point2Y);
+            double s2 = Side(Point3X, Point3Y, Point2X, Point2Y);
+            double s3 = Side(Point1X, Point1Y, Point3X, Point3Y);
+            double s = (s1 + s2 + s3) / 2;
+            return Math.Sqrt((s-s1) * (s-s2) * (s - s3) * s);
+        }
+
+        public double RadiusOfCircumcircle()// 計算外接圓半徑
+        {
+            double s1 = Side(Point1X, Point1Y, Point2X, Point2Y);
+            double s2 = Side(Point3X, Point3Y, Point2X, Point2Y);
+            double s3 = Side(Point1X, Point1Y, Point3X, Point3Y);
+            double cosA = (s1 * s1 + s2 * s2 - s3 * s3) / (2 * s1 * s2);
+            double sinA = Math.Sqrt(1 - cosA * cosA);
+            return s3/(2*sinA);
+
+        }
+        public bool IsRight()// 檢查三角形是否為直角
+        {
+            double s1 = Side(Point1X, Point1Y, Point2X, Point2Y);
+            double s2 = Side(Point3X, Point3Y, Point2X, Point2Y);
+            double s3 = Side(Point1X, Point1Y, Point3X, Point3Y);
+            if(s1*s1+s2*s2-s3*s3<1e-10|| s1 * s1 + s3 * s3 - s2 * s2 < 1e-10|| s3 * s3 + s2 * s2 - s1 * s1 < 1e-10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
